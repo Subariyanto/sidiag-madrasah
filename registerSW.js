d5b8fb901a158c38433075a5feaec75772ccce7a
Subariyanto <subariyantoss2@gmail.com>
2026-07-16 05:06:50 +0700

    Deploy with Supabase credentials [skip ci]

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sidiag-madrasah/sw.js', { scope: '/sidiag-madrasah/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
